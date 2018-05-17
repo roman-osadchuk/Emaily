f11551a069b41bf6e319a04af55fb411e94b2d38
Roman Osadchuk <uaromik@gmail.com>
2018-05-17 13:58:29 +0300

    basic authentication completed

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -16,6 +16,18 @@ module.exports = app => {
   app.get('/auth/google/callback', passport.authenticate('google'));
   
   
+  app.get('/api/logout', (req, res) => {
+    req.logout();
+    res.send(req.user);
+  });
+  
+  
+  app.get('/api/current_user', (req, res) => {
+    res.send(req.user);
+  });
+  
+  
+  
   // app.get('/auth/google/callback', passport.authenticate('google'), // complete the authenticate using the google strategy
   //   (err, req, res, next) => { // custom error handler to catch any errors, such as TokenError
   //     if (err.name === 'TokenError') {
--- services/passport.js
+++ services/passport.js
@@ -5,12 +5,38 @@ const keys = require('../config/keys');
 
 const User = mongoose.model('users');
 
+// this first argument 'user' is what we receive from done() in 'GoogleStrategy'
+passport.serializeUser((user, done) => {
+  // here 'null' represents error object, but as we definitely have user so we cannot receive err there so we put 'null'
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  })
+});
+
+
 passport.use(
   new GoogleStrategy({ 
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback'
   }, (accessToken, refreshToken, profile, done) => {
-    new User({ googleId: profile.id }).save();
+    User.findOne({ googleId: profile.id })
+      .then((existingUser) => {
+        if(existingUser) {
+          // we already have a record with given profile id
+          
+          done(null, existingUser);
+          
+        } else {
+          new User({ googleId: profile.id })
+            .save()
+            .then(user => done(null, user));
+        }
+      })
+    
   })
 );
