633cfa8effdcb728264d79090c01b92bb5f9e9d4
Roman Osadchuk <uaromik@gmail.com>
2018-05-17 15:39:42 +0300

    basic auth completed

---
+++ config/prod.js
@@ -0,0 +1,8 @@
+// production keys can be obtained from environment
+
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY
+};
