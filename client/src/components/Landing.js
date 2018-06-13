e6ec984ccb21e6087ad49932ed61f6376d8d61a4
Roman Osadchuk <uaromik@gmail.com>
2018-06-13 19:53:17 +0300

    text refactor

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -13,7 +13,8 @@ class Landing extends Component {
       <div style={{textAlign: 'center'}}>
         <h1>Emaily</h1>
         Collect feedbacks from your users
-        <span>this text has been added in temp branch</span>
+        <span>this text has been added in temp </span>
+        So new text
       </div>
     );
   }
