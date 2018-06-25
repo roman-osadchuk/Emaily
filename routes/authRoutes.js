6f7e74f84517931916187982297fa9aa759e1692
Roman Osadchuk <osadchukroman94@gmail.com>
2018-06-25 15:43:05 +0300

    commented root route on server

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,12 +1,12 @@
 const passport = require('passport');
 
 module.exports = app => {
-  
-  app.get('/', (err, req, res) => {
-      res.json({ "greeting": "hello"});
-    }
-  );
-  
+
+  // app.get('/', (err, req, res) => {
+  //     res.json({ "greeting": "hello"});
+  //   }
+  // );
+
   app.get('/auth/google', passport.authenticate('google', {
       scope: ['profile', 'email']
     })
@@ -16,20 +16,20 @@ module.exports = app => {
   app.get('/auth/google/callback', passport.authenticate('google'), (req, res) => {
     res.redirect('/surveys');
   });
-  
-  
+
+
   app.get('/api/logout', (req, res) => {
     req.logout();
     res.redirect('/');
   });
-  
-  
+
+
   app.get('/api/current_user', (req, res) => {
     res.send(req.user);
   });
-  
-  
-  
+
+
+
   // app.get('/auth/google/callback', passport.authenticate('google'), // complete the authenticate using the google strategy
   //   (err, req, res, next) => { // custom error handler to catch any errors, such as TokenError
   //     if (err.name === 'TokenError') {
@@ -42,8 +42,7 @@ module.exports = app => {
   //     res.redirect('/');
   //   }
   // );
-  
-  
-  
-};
 
+
+
+};
