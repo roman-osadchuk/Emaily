164cd6e70937515ac06d55be0822190f00cc44de
Osadchuk Roman <uaromik@gmail.com>
2018-07-20 14:24:05 +0300

    handling sendgrid responces and saving them to db

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -10,25 +10,61 @@ const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 const Survey = mongoose.model('surveys');
 
 module.exports = app => {
-    app.get('/api/surveys/thanks', (req, res) => {
-        res.send('Thanks for voting!');
+    app.get('/api/surveys', requireLogin, async (req, res) => {
+
+        const surveys = await Survey.find({ _user: req.user.id })
+            .select({ recipients: false })
+
+        res.send(surveys);
+    });
+
+
+    app.get('/api/surveys/:surveyId/:choice', (req, res) => {
+        res.send('Thank you for voting!');
     });
 
     app.post('/api/surveys/webhooks', (req, res) => {
-        const events = req.body.map(event => {
-            const pathname = new URL(event.url).pathname;
-            const p = new Path('/api/surveys/:surveyId/:choice');
-            const match = p.test(pathname);
-            if (match) {
-                return { email: event.email, serveyId: match.surveyId, choice: match.choice };
-            }
-        })
+        const p = new Path('/api/surveys/:surveyId/:choice');
 
-        // return elements except null and undefined
-        const compactEvents = _.compact(events);
-        const uniqueEvents = _.uniqBy(compactEvents, 'email', 'surveyId');
+        _.chain(req.body)
+            .map(event => {
+                const match = p.test(new URL(event.url).pathname);
+                if (match) {
+                    return { email: event.email, serveyId: match.surveyId, choice: match.choice };
+                }
+            })
+            .compact()
+            .uniqBy('email', 'surveyId')
+            .each(({ surveyId, email, choice }) => {
+                Survey.updateOne(
+                    {
+                        _id: surveyId,
+                        recipients: {
+                            $elemMatch: { email: email, responded: false }
+                        }
+                    },
+                    {
+                        $inc: { [choice]: 1 },
+                        $set: { 'recipients.$.responded': true },
+                        lastResponded: new Date()
+                    }
+                ).exec();
+            })
+            .value();
 
-        console.log(uniqueEvents);
+        // The same code as above but chained with lodash
+        // const events = req.body.map(event => {
+        //     const match = p.test(new URL(event.url).pathname);
+        //     if (match) {
+        //         return { email: event.email, serveyId: match.surveyId, choice: match.choice };
+        //     }
+        // })
+        //
+        // // return elements except null and undefined
+        // const compactEvents = _.compact(events);
+        // const uniqueEvents = _.uniqBy(compactEvents, 'email', 'surveyId');
+
+
 
     });
 
